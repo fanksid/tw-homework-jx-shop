5f9c84bf42977e3caebe7f0c116049a1b0caf1a7
superxc <superxcgm@gmail.com>
2018-05-11 09:37:15 +0800

    first commit

---
+++ src/main/java/com/superxc/jxshop/JxShopApplication.java
@@ -0,0 +1,12 @@
+package com.superxc.jxshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JxShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JxShopApplication.class, args);
+	}
+}
