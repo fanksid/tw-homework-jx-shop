3777c743f904fcaa91088c43976a02fddf2a3374
superxc <superxcgm@gmail.com>
2018-05-19 10:18:36 +0800

    db connection use utf8, add test_data to flyway migration

---
+++ src/main/resources/db/migration/V2__Insert_test_data.sql
@@ -0,0 +1,4 @@
+INSERT INTO product(name, description, price) VALUES ('方便面', '很好吃，康师傅', 4.8);
+INSERT INTO product(name, description, price) VALUES ('方便面', '不好吃', 2.5);
+INSERT INTO product(name, description, price) VALUES ('电脑', '坚如磐石', 5999);
+INSERT INTO product(name, description, price) VALUES ('怡宝水', '550ml', 1.5);
